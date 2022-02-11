2bfe076f55d6a38f7f48c4df8214c793bd1b11cb
waterdragon <tndyd5390@naver.com>
2022-02-12 05:02:14 +0900

    entity config

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@DiscriminatorValue("M")
+public class Movie extends Item {
+
+    private String director;
+    private String actor;
+
+}
